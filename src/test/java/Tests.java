302988d1d8cca1827552dcc1508d280a6e5e27f1
adilaattokur <adilaattokur@yahoo.com>
2023-10-01 21:54:26 -0500

    fixed

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,7 +3,7 @@ package src.test.java;
 public class Tests {
     public static void main(String[] args) {
         System.out.println("added main method");
-        System.out.println("AIKA added method");
+        System.out.println("aika added method");
 
     }
 
