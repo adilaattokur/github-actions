1ecd10aca1d3d4e217fb09bab03ab3d66991c59d
adilaattokur <adilaattokur@yahoo.com>
2023-10-01 21:52:42 -0500

    aika test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,6 +3,8 @@ package src.test.java;
 public class Tests {
     public static void main(String[] args) {
         System.out.println("added main method");
+        System.out.println("AIKA added method");
+
     }
 
 }
